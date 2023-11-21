e43c760b2056afed37d6038a2a9ea18ab54cf889
Jose Bohorquez <bd567358546@gmail.com>
2023-11-21 00:11:04 -0500

    tipos de datos en dart

---
+++ 2-tipos-de-datos.dart
@@ -0,0 +1,15 @@
+/*
+Lenguajes de programación fuertemente tipados:
+C#,Ada,Modula-2,Pascal,Java,Haskell,ML,TypeScript,Swift,Go
+
+Lenguajes de programación dinámicamente tipados:
+Python,JavaScript,Ruby,PHP,Perl,Lisp,Smalltalk,Erlang,Elixir
+
+tipos de datos en dart:
+int, double => num
+string,bool => dynamic
+ */
+void main()
+{
+
+}
